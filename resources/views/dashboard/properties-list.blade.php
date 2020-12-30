2ab9aa16b477af7e58ddcf2d1acd192a2124d245
Marco Marassi <thtg88@users.noreply.github.com>
2020-12-30 21:09:01 +0000

    Refactoring

--- resources/views/dashboard/properties-list.blade.php
+++ resources/views/dashboard/properties-list.blade.php
@@ -1,11 +1,10 @@
 <div class="mt-4 grid grid-cols-1 divide-y divide-gray-500">
     @foreach ($properties as $property)
         <div class="py-2">
-            <x-link href="{{ route('properties.show', $property) }}">
+            <x-link href="{{ route('properties.show', $property) }}" class="text-base">
                 {{ $property->title ?? $property->url }}
             </x-link>
-            -
-            Likes: {{ $property->getLikes()->count() }}
+            - Likes: {{ $property->getLikes()->count() }}
             Dislikes: {{ $property->getDislikes()->count() }}
         </div>
     @endforeach
