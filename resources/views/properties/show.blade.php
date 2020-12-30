40bae75dd3ebd59bee7fe831f47fc9db114809e4
Marco Marassi <thtg88@users.noreply.github.com>
2020-12-30 00:46:17 +0000

    Show list of preferences

--- resources/views/properties/show.blade.php
+++ resources/views/properties/show.blade.php
@@ -34,6 +34,31 @@
         </p>
     </x-card>
 
+    @if (
+        (
+            $model->property_preferences->count() > 0 &&
+            $user_preference === null
+        ) ||
+        (
+            $model->property_preferences->count() > 1 &&
+            $user_preference !== null
+        )
+    )
+        <x-card>
+            @foreach ($model->property_preferences as $property_preference)
+                <p class="mt-2">
+                    @if ($property_preference->is_liked === true)
+                        <x-icons.check class="w-10 h-10 inline mr-2" />
+                    @else
+                        <x-icons.times class="w-10 h-10 inline mr-2" />
+                    @endif
+                    {{ $property_preference->user->name }}
+                    {{ $property_preference->is_liked === false ? 'dis' : '' }}liked this property
+                </p>
+            @endforeach
+        </x-card>
+    @endif
+
     @if ($user_preference !== null)
         <x-card>
             <p class="mt-2">
