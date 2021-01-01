d20aeef23212508adb813d27d864cdc85362f4d5
Marco Marassi <thtg88@users.noreply.github.com>
2021-01-01 19:42:38 +0000

    Make user_groups inviter_id nullable

--- database/migrations/2020_12_30_143438_create_user_groups_table.php
+++ database/migrations/2020_12_30_143438_create_user_groups_table.php
@@ -17,7 +17,7 @@ class CreateUserGroupsTable extends Migration
             $table->id();
             $table->foreignId('group_id')->index();
             $table->foreignId('user_id')->index();
-            $table->foreignId('inviter_id')->index();
+            $table->foreignId('inviter_id')->nullable()->index();
             $table->timestamp('invited_at')->nullable();
             $table->timestamp('accepted_at')->nullable();
             $table->timestamps();
