5a9e878979053055a46745f5b01334b8f8f0effe
Marco Marassi <thtg88@users.noreply.github.com>
2021-01-02 01:01:10 +0000

    Added view on website button on property show

---
+++ resources/views/components/buttons/primary-button-link.blade.php
@@ -0,0 +1,5 @@
+<a href="{{ $href }}" {{ $attributes->merge([
+    'class' => 'inline-flex items-center px-4 py-2 border border-gray-800 bg-transparent rounded-md font-semibold text-xs text-gray-800 uppercase tracking-widest hover:bg-gray-700 hover:text-white active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150',
+]) }}>
+    {{ $slot }}
+</a>
--- resources/views/properties/show.blade.php
+++ resources/views/properties/show.blade.php
@@ -8,6 +8,15 @@
         <div class="mt-4 flex-shrink-0 flex md:mt-0 md:ml-4">
             <form action="{{ route('properties.reprocess', $model) }}" method="post">
                 @csrf
+                <x-buttons.primary-button-link href="{{ $model->url }}" target="_blank" rel="noopener noreferrer">
+                    View on website
+                    <x-icons.external-link class="w-4" />
+                </x-buttons.primary-button-link>
+            </form>
+        </div>
+        <div class="mt-4 flex-shrink-0 flex md:mt-0 md:ml-4">
+            <form action="{{ route('properties.reprocess', $model) }}" method="post">
+                @csrf
                 <x-buttons.primary-button type="submit" data-confirm="Are you sure you want to reprocess this property details? This will overwrite existing information">
                     Reprocess
                 </x-buttons.primary-button>
