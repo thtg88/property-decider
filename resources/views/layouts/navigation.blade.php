a00caffc3bf5f687e3bacb23280a662e8dcc1724
Marco Marassi <thtg88@users.noreply.github.com>
2021-01-03 21:50:07 +0000

    Navigation refactored

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,12 +10,12 @@
                     </a>
                 </div>
 
-                <!-- Navigation Links -->
-                <!-- <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                {{--<!-- Navigation Links -->--}}
+                {{--<!-- <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                </div> -->
+                </div> -->--}}
             </div>
 
             <!-- Settings Dropdown -->
@@ -37,6 +37,7 @@
                         <x-dropdown-link href="{{ route('profile.edit') }}">
                             {{ __('Profile') }}
                         </x-dropdown-link>
+
                         <!-- Authentication -->
                         <form method="POST" action="{{ route('logout') }}">
                             @csrf
@@ -66,11 +67,11 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <!-- <div class="pt-2 pb-3 space-y-1">
+        {{--<!-- <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
-        </div> -->
+        </div> -->--}}
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
@@ -90,9 +91,10 @@
                 <form method="POST" action="{{ route('logout') }}">
                     @csrf
 
-                    <x-responsive-nav-link :href="route('logout')"
-                            onclick="event.preventDefault();
-                                        this.closest('form').submit();">
+                    <x-responsive-nav-link
+                        :href="route('logout')"
+                        onclick="event.preventDefault(); this.closest('form').submit();"
+                    >
                         {{ __('Logout') }}
                     </x-responsive-nav-link>
                 </form>
