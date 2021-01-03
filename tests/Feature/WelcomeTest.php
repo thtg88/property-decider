e5ae3f346fe795fe4515a20cf9600941b76dad93
Marco Marassi <thtg88@users.noreply.github.com>
2021-01-03 19:13:25 +0000

    Added welcome test

---
+++ tests/Feature/WelcomeTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+
+class WelcomeTest extends TestCase
+{
+    /** @test */
+    public function successful_get_as_guest(): void
+    {
+        $response = $this->actingAs()->get($this->getRoute());
+        $response->assertStatus(200)
+            ->assertViewIs('welcome.main');
+    }
+
+    /** @test */
+    public function successful_authenticated(): void
+    {
+        $user = User::factory()->create();
+        $response = $this->actingAs($user)->get($this->getRoute());
+        $response->assertStatus(200)
+            ->assertViewIs('welcome.main');
+    }
+
+    /**
+     * Return the route to use for these tests.
+     *
+     * @param array $parameters
+     * @return string
+     */
+    public function getRoute(array $parameters = []): string
+    {
+        return '/';
+    }
+}
